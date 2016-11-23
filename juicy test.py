e73d554984af7401e396ac38c3615c01926cc187
lee jung uk <dlwjddnr93@naver.com>
2016-11-23 23:24:57 +0900

    REFATORING

--- juicy test.py
+++ juicy test.py
@@ -3,6 +3,7 @@ import math
 sys.path.append('../2dgample')
 
 import random
+from BackGround import BackGround
 from pico2d import *
 
 space = None
@@ -21,26 +22,7 @@ break_status1 = None
 #KeyDown = {LEFT,UP,RIGHT,DOWN,NORMAL}
 
 
-class BackGround:
-    image = None
 
-    def draw(self):
-        self.image.draw(self.x,self.y,800,600)
-
-    def __init__(self):
-        self.x, self.y = 400,300   # init positon player
-        self.frame = random.randint(0, 7)
-        self.run_frames = 0
-        self.stand_frames = 0
-        self.state = 0
-        if BackGround.image == None:
-            BackGround.image = load_image('animated_bg12 copy1.png')
-
-    def update(self):
-        self.x -= 20
-        self.frame = (self.frame + 1) % 60
-        if self.x <= -800:
-            self.x = 800
 
 
 
